05409518cdef77cf5a6a378531b44fbffb2079ba
xinove <xinovebig@gmail.com>
2022-05-19 11:36:14 -0300

    minishellzinho

---
+++ Minishellzinho/main.c
@@ -0,0 +1,200 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "../libft/libft.h"
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
+char **lsh_split_line(char *line);
+void	minizinho_loop(void);
+char	*mini_read_line(void);
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+int lsh_execute(char **args);
+
+char *builtin_str[] = {
+  "cd",
+  "help",
+  "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+  &lsh_cd,
+  &lsh_help,
+  &lsh_exit
+};
+
+int lsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+int	main(int argc, char *argv[])
+{
+	minizinho_loop();
+	return (EXIT_SUCCESS);
+}   
+
+void	minizinho_loop(void)
+{
+	char	*line;
+	char	**args;
+	int		status;
+
+	do {
+		printf("> ");
+		line = mini_read_line();
+		args = lsh_split_line(line);
+		status = lsh_execute(args);
+
+		free(line);
+		free(args);
+	} while (status);
+}
+
+char	*mini_read_line(void)
+{
+	int		buf_size = BUFFER_SIZE;
+	int		i;
+	int		c; // TODO test int as char and (char) EOF
+	char	*buffer;
+
+	i = 0;
+	buf_size = BUFFER_SIZE;
+	buffer = malloc(sizeof(char) * buf_size);
+
+	if (!buffer)
+	{
+		fprintf(stderr, "error allocating\n");
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		c = getchar();
+		if (c == EOF || c == '\n')
+		{
+			buffer[i] = '\0';
+			return (buffer);
+		}
+		buffer[i] = c;
+		i++;
+		if (i >= buf_size)
+		{
+			buf_size += BUFFER_SIZE;
+			buffer = realloc(buffer, buf_size);
+			if (!buffer)
+			{
+				fprintf(stderr, "error allocating\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+
+char **lsh_split_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+int lsh_launch(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+      perror("lsh");
+    exit(EXIT_FAILURE);
+  } else if (pid < 0)
+    perror("lsh");
+  else
+	{
+    do
+		{
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+  return (1);
+}
+
+int lsh_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL) {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return lsh_launch(args);
+}
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+
+/*
+  Builtin function implementations.
+*/
+int lsh_cd(char **args)
+{
+  if (args[1] == NULL)
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  else
+    if (chdir(args[1]) != 0)
+      perror("lsh");
+  return (1);
+}
+
+int lsh_help(char **args)
+{
+  int i;
+  printf("Stephen Brennan's LSH\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+  for (i = 0; i < lsh_num_builtins(); i++)
+    printf("  %s\n", builtin_str[i]);
+  printf("Use the man command for information on other programs.\n");
+  return (1);
+}
+
+int lsh_exit(char **args)
+{
+  return (0);
+}
